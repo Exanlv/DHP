35a9627de9b274ae38aeef5ed1472f10a44e0ee1
Exan <exan@landviz.nl>
2020-05-22 15:53:57 +0200

    Add get reactions option

---
+++ src/RestClient/Channel/Classes/GetReactionsOptions.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DHP\RestClient\Channel\Classes;
+
+class GetReactionsOptions
+{
+    /**
+     * @var string
+     */
+    public $before;
+
+    /**
+     * @var string
+     */
+    public $after;
+
+    /**
+     * @var int
+     */
+    public $limit;
+}
